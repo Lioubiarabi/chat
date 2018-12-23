31bcfa53cae6b57db416e30f59a9d10579f7dbd6
Lioubiarabi <46072439+Lioubiarabi@users.noreply.github.com>
2018-12-23 10:19:42 +0000

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+ var config = {
+ apiKey: "AIzaSyCw2kYwg0BZeHy4LCDIwqFeheV1h3dBsbk",
+ authDomain: "arabi-aad8b.firebaseapp.com",
+ databaseURL: "https://arabi-aad8b.firebaseio.com",
+ projectId: "arabi-aad8b",
+ storageBucket: "arabi-aad8b.appspot.com",
+ messagingSenderId: "613812918510"
+ };
+ firebase.initializeApp(config);
